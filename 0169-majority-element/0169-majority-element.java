f4b5f040acf2eedf12e59d55ed27073f07e367e9
Vivek5499 <vivekkumar175499@gmail.com>
2026-07-31 01:12:08 +0530

    Time: 1511 ms (7.21%), Space: 55.7 MB (46.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,7 +4,7 @@ class Solution {
         for(int j = 0; j < n; j++){
             int count = 0;
             for(int i = j; i < n; i++){
-              if(nums[i] == nums[j]) ++count;
+              if(nums[i] == nums[j]) count++;
             }
               if(count > n/2) return nums[j];
             
