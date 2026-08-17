bc075bfb74652761b3a26e734ea4b7478f6417f1
Vivek5499 <vivekkumar175499@gmail.com>
2026-08-18 02:44:38 +0530

    Time: 1 ms (98.26%), Space: 43.2 MB (72.42%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int score = 0;
+        int sum = 0;
+        for(int i = 0; i < s.length()-1; i++){
+            sum = Math.abs(s.charAt(i) - s.charAt(i+1));
+            score += sum;
+        }
+        return score;
+    }
+}
